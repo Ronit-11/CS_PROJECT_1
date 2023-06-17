72fcdcbc039752661cb609d9b44da987f8b6e258
Ronit-11 <98330660+Ronit-11@users.noreply.github.com>
2023-06-17 22:03:33 +0300

    working gmail auth

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ URL('Images/SERV.png') }}" alt="resources/views/components/application-logo.blade.php" width="125px"/>
+<img src="{{ URL('Images/SERV.png') }}" alt="resources/views/components/application-logo.blade.php(not header)" width="125px"/>
--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ URL('Images/SERV.png') }}" alt="resources/views/components/application-mark.blade.php" width="125px"/>
+<img src="{{ URL('Images/SERV.png') }}" alt="resources/views/components/application-mark.blade.php(top left logo)" width="90px"/>
